74e46a52c8003e0fdfd3d7b008aa1fef0922cea7
Arab45 <horlakunleolayiwola@gmail.com>
2025-05-17 13:03:36 -0700

    add Mongoose integration for Course module with schema definition

--- src/course/course.module.ts
+++ src/course/course.module.ts
@@ -2,9 +2,11 @@ export { Module } from '@nestjs/common';
 import { Module } from '@nestjs/common';
 import { CourseController } from './course.controller';
 import { CourseService } from './course.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Course, courseSchema } from 'src/schema/course.schema';
 
 @Module({
-    imports: [],
+    imports: [MongooseModule.forFeature([{ name: Course.name, schema: courseSchema}])], // Assuming you have a CourseSchema defined
     controllers: [CourseController],
     providers: [CourseService],
 })
